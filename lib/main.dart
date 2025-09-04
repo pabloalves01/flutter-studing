84dcbba14e954e8573fab079a21b0fbc60395f9b
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-09-04 12:43:29 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,37 +7,15 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: Text('Tarefas', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.black87,
-        ),
-        body: InitialScreen(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(opacidade ? Icons.visibility : Icons.visibility_off),
-          onPressed: () {
-            setState(() {
-              opacidade = !opacidade;
-            });
-          },
-        ),
-      ),
+      home: InitialScreen(),
     );
   }
 }
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -12,43 +12,58 @@ class _InitialScreenState extends State<InitialScreen> {
   bool opacidade = true;
   @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(
-      opacity: opacidade ? 1 : 0,
-      duration: Duration(milliseconds: 500),
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Task(
-            'Teste 1',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            1,
-          ),
-          Task(
-            'Teste 2',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            2,
-          ),
-          Task(
-            'Teste 4',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            4,
-          ),
-          Task(
-            'Teste 5',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            5,
-          ),
-          Task(
-            'Teste 6',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            3,
-          ),
-          Task(
-            'Teste 7',
-            'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
-            2,
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Text('Tarefas', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black87,
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1 : 0,
+        duration: Duration(milliseconds: 500),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Task(
+              'Teste 1',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              1,
+            ),
+            Task(
+              'Teste 2',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              2,
+            ),
+            Task(
+              'Teste 4',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              4,
+            ),
+            Task(
+              'Teste 5',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              5,
+            ),
+            Task(
+              'Teste 6',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              3,
+            ),
+            Task(
+              'Teste 7',
+              'https://site-inkor.s3.amazonaws.com/imagens/tinta-podium-6888fc3e372fa.png',
+              2,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(opacidade ? Icons.visibility : Icons.visibility_off),
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
       ),
     );
   }

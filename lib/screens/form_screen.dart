ee8801c643c2463d071d95ebedb01252b6934f5a
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-10-21 22:43:52 -0300

    feat: validando dados dos TextFormField com o Form pai, atravez do estado do Form e usando validate

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -13,109 +15,159 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
+  // fica de olho quando as informacoes (validacoes) sao preenchidas atravez do formstate do formulario que envolve os textformfield
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Container(),
-        title: const Text('Nova Tarefa', style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.black87,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: Container(
-          // height: 650,
-          // width: 375,
-          // decoration: BoxDecoration(
-          //   color: Colors.black12,
-          //   borderRadius: BorderRadius.circular(10),
-          //   border: Border.all(width: 1, color: Colors.black26),
-          // ),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Digite o nome',
-                    fillColor: Colors.white70,
-                    filled: true,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text(
+            'Nova Tarefa',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.black87,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8),
+          child: SingleChildScrollView(
+            child: Container(
+              // height: 650,
+              // width: 375,
+              // decoration: BoxDecoration(
+              //   color: Colors.black12,
+              //   borderRadius: BorderRadius.circular(10),
+              //   border: Border.all(width: 1, color: Colors.black26),
+              // ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: TextFormField(
+                      controller: nameController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Nome não pode ser vazio';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Digite o nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  controller: difficultyController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Dificuldade',
-                    fillColor: Colors.white70,
-                    filled: true,
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: TextFormField(
+                      controller: difficultyController,
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) < 1 ||
+                            int.parse(value) > 5) {
+                          return 'Dificuldade deve ser entre 1 e 5';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  onChanged: (text) {
-                    setState(() {}); // Necessário apenas aqui para atualizar preview da imagem
-                  },
-                  controller: imageController,
-                  keyboardType: TextInputType.url,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Imagem',
-                    fillColor: Colors.white70,
-                    filled: true,
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: TextFormField(
+                      onChanged: (text) {
+                        setState(
+                          () {},
+                        ); // Necessário apenas aqui para atualizar preview da imagem
+                      },
+                      controller: imageController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Imagem não pode ser vazia';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.url,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
+
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: imageController.text.isNotEmpty
+                          ? Image.network(
+                              imageController.text,
+                              fit: BoxFit.cover,
+                              errorBuilder:
+                                  (
+                                    BuildContext context,
+                                    Object expection,
+                                    StackTrace? stackTrace,
+                                  ) {
+                                    return Icon(
+                                      Icons.error,
+                                      color: Colors.white,
+                                    );
+                                  },
+                              // errorBuilder: (context, error, stackTrace) {
+                              //   return Icon(Icons.error, color: Colors.white);
+                              // },
+                              // loadingBuilder: (context, child, loadingProgress) {
+                              //   if (loadingProgress == null) return child;
+                              //   return Center(
+                              //     child: CircularProgressIndicator(
+                              //       value: loadingProgress.expectedTotalBytes != null
+                              //           ? loadingProgress.cumulativeBytesLoaded /
+                              //               loadingProgress.expectedTotalBytes!
+                              //           : null,
+                              //     ),
+                              //   );
+                              // },
+                            )
+                          : Icon(Icons.image, color: Colors.white),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // fica de olho no estado do formulario para verificar se o formulario foi validado,
+                        //pq conforme eu preencho os campos o estado muda e o validate verifica se o formulario foi validado
+                        print('Formulario validado');
+                      }
+                    },
+                    child: Text('Adicionar'),
+                  ),
+                ],
               ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(width: 2, color: Colors.blue),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: imageController.text.isNotEmpty
-                      ? Image.network(
-                          imageController.text,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Icon(Icons.error, color: Colors.white);
-                          },
-                          loadingBuilder: (context, child, loadingProgress) {
-                            if (loadingProgress == null) return child;
-                            return Center(
-                              child: CircularProgressIndicator(
-                                value: loadingProgress.expectedTotalBytes != null
-                                    ? loadingProgress.cumulativeBytesLoaded /
-                                        loadingProgress.expectedTotalBytes!
-                                    : null,
-                              ),
-                            );
-                          },
-                        )
-                      : Icon(Icons.image, color: Colors.white),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print(nameController.text);
-                  print(int.parse(difficultyController.text));
-                  print(imageController.text);
-                },
-                child: Text('Adicionar'),
-              ),
-            ],
+            ),
           ),
         ),
       ),

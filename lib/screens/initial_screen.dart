8bda7ddac6b5123ab57a98d5028a47d8e6739c77
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-12-13 18:12:28 -0300

    adicionado list view builder para pegar as tarefas e mostrar na tela

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/task.dart';
+import 'package:flutter_application_1/data/task_dao.dart';
 import 'package:flutter_application_1/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
@@ -30,22 +31,38 @@ class _InitialScreenState extends State<InitialScreen> {
           ),
         ],
       ),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: Duration(milliseconds: 500),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            Task('Teste 1', 'assets/images/tinta-podium.png', 1),
-            Task('Teste 2', 'assets/images/tinta-podium.png', 2),
-            Task('Teste 4', 'assets/images/tinta-podium.png', 4),
-            Task('Teste 5', 'assets/images/tinta-podium.png', 5),
-            Task('Teste 6', 'assets/images/tinta-podium.png', 3),
-            Task('Teste 7', 'assets/images/tinta-podium.png', 2),
-            SizedBox(height: 80),
-          ],
+      body: Padding(
+        padding: EdgeInsets.only(top: 8.0, bottom: 70.0),
+        child: FutureBuilder<List<Task>>(
+          future: TaskDao().findAll(),
+          builder: (context, snapshot) {
+            List<Task>? items = snapshot.data;
+            return ListView.builder(
+              itemCount: items!.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Task task = items[index];
+                return task;
+              },
+            );
+          },
         ),
       ),
+      // body: AnimatedOpacity(
+      //   opacity: opacidade ? 1 : 0,
+      //   duration: Duration(milliseconds: 500),
+      //   child: ListView(
+      //     scrollDirection: Axis.vertical,
+      //     children: [
+      //       Task('Teste 1', 'assets/images/tinta-podium.png', 1),
+      //       Task('Teste 2', 'assets/images/tinta-podium.png', 2),
+      //       Task('Teste 4', 'assets/images/tinta-podium.png', 4),
+      //       Task('Teste 5', 'assets/images/tinta-podium.png', 5),
+      //       Task('Teste 6', 'assets/images/tinta-podium.png', 3),
+      //       Task('Teste 7', 'assets/images/tinta-podium.png', 2),
+      //       SizedBox(height: 80),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
